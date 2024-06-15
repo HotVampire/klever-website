712b4192ee17bdc85ba948ca4d3f93d69f1013b0
Maxim Orlov <orlovv200443@gmail.com>
2024-06-15 17:00:39 +0300

    initial Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/templates/**/*.html',
+    './templates/**/*.html',
+    './static/src/**/*.js',
+    './static/src/**/*.jsx',
+    './static/src/**/*.ts',
+    './static/src/**/*.tsx',
+    './static/src/**/*.vue',
+    './static/src/**/*.html',
+    './static/src/**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
