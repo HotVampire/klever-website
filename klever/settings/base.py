17259d2f9d5d1913b31591efb3a6405da1c4b92c
Domitorii Erokhin <101994645+Darkleizing@users.noreply.github.com>
2024-06-19 19:30:25 +0300

    Подготовка к диплому

--- klever/settings/base.py
+++ klever/settings/base.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "wagtail",
     "modelcluster",
     "taggit",
+    "django_extensions",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
